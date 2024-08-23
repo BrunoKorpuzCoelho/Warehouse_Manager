5fb393bb6cba8d79977882a19bbf29d4cf43b38f
Bruno Coelho <138626948+BrunoKorpuzCoelho@users.noreply.github.com>
2024-08-23 17:52:59 +0100

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,32 @@ const toggle = document.getElementById('darkModeToggle');
                 localStorage.setItem('darkMode', 'disabled');
             }
         });
+
+ document.addEventListener("DOMContentLoaded", function() {
+       let lastScrollTop = 0;
+       const header = document.querySelector('.header-dashboard');
+    
+       window.addEventListener('scroll', function() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            header.classList.add('header-hidden');
+        } else {
+            header.classList.remove('header-hidden');
+        }
+                
+        lastScrollTop = scrollTop;
+    });
+});
+
+var userType = "{{ user.user_type }}";
+
+document.getElementById("new-user-link").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if (userType === "Admin" || userType === "Manager") {
+        window.location.href = "";   // COLOCAR AQUI O LINK PARA A PAGINA DE CRIACAO DE USUARIOS
+    } else {
+        window.location.href = "";  //COLOCAR AQUI O LINK PARA A PAGINA DE SEM PERMISSAO PARA ACESSAR A PAGINA
+    }
+});
